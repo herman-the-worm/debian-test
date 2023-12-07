e94941e26b2bd753dada31352562b9f257004c7e
Your Name <herman-the-worm@users.noreply.github.com>
2023-12-07 15:28:53 -0700

    adjust dockerfile and submit tests

--- install-chrome.sh
+++ install-chrome.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 set -e
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update -y && apt-get install -y \
-    --fix-missing \
-     google-chrome-stable
+# Download and install Google Chrome
+CHROME_DEB_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+CHROME_DEB_PATH="/tmp/google-chrome-stable_current_amd64.deb"
+curl -o $CHROME_DEB_PATH $CHROME_DEB_URL
+apt update -y \
+    && apt install -y $CHROME_DEB_PATH
+rm -f $CHROME_DEB_PATH
 
-#google-chrome --headless --dump-dom https://www.google.com --remote-debugging-port=9222
+## Get Chrome versions information
+#CHROME_PLATFORM="linux64"
+#CHROME_VERSIONS_URL="https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build-with-downloads.json"
+#CHROME_VERSIONS_JSON=$(curl -fsSL "${CHROME_VERSIONS_URL}")
 #
-#google-chrome --headless --no-sandbox --disable-gpu --dump-dom https://www.google.com
+## Extract Chrome version from the JSON
+#CHROME_VERSION=$(echo "${CHROME_VERSIONS_JSON}" | jq -r '.latest_patch_version')
+#
+## Download and install ChromeDriver
+#CHROMEDRIVER_VERSION=$(echo "${CHROME_VERSIONS_JSON}" | jq -r '.builds["'"$CHROME_VERSION"'"].version')
+#CHROMEDRIVER_URL=$(echo "${CHROME_VERSIONS_JSON}" | jq -r '.builds["'"$CHROME_VERSION"'"].downloads.chromedriver[] | select(.platform=="'"${CHROME_PLATFORM}"'").url')
+#CHROMEDRIVER_DIR="/usr/local/share/chromedriver-linux64"
+#CHROMEDRIVER_BIN="$CHROMEDRIVER_DIR/chromedriver"
+#
+#echo "Installing ChromeDriver version $CHROMEDRIVER_VERSION"
+#curl -fsSL -o /tmp/chromedriver.zip $CHROMEDRIVER_URL
+#mkdir -p $CHROMEDRIVER_DIR
+#unzip -qq /tmp/chromedriver.zip -d $CHROMEDRIVER_DIR
+#rm /tmp/chromedriver.zip
+#
+#chmod +x $CHROMEDRIVER_BIN
+#ln -s $CHROMEDRIVER_BIN /usr/bin/chromedriver
+#
+## Set CHROMEWEBDRIVER environment variable
+#echo 'export CHROMEWEBDRIVER='${CHROMEDRIVER_DIR} >> /etc/environment
+#
+## Download and install Chromium
+#CHROME_REVISION=$(echo "${CHROME_VERSIONS_JSON}" | jq -r '.builds["'"$CHROME_VERSION"'"].revision')
+#CHROMIUM_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${CHROME_REVISION}%2Fchrome-linux.zip?alt=media"
+#CHROMIUM_DIR="/usr/local/share/chromium"
+#CHROMIUM_BIN="${CHROMIUM_DIR}/chrome-linux/chrome"
+#
+#echo "Installing Chromium revision $CHROME_REVISION"
+#curl -fsSL -o /tmp/chromium.zip $CHROMIUM_URL
+#mkdir -p $CHROMIUM_DIR
+#unzip -qq /tmp/chromium.zip -d $CHROMIUM_DIR
+#rm /tmp/chromium.zip
+#
+#ln -s $CHROMIUM_BIN /usr/bin/chromium
+#ln -s $CHROMIUM_BIN /usr/bin/chromium-browser
